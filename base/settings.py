cfea526c76699382dc6beb4c9191ee4b5d463c72
Alexandr Pogrebnyuk <nani.lennauchpage@gmail.com>
2025-11-26 18:24:29 +0200

    added .env to .gitignore

--- base/settings.py
+++ base/settings.py
@@ -34,14 +34,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tbf1@720q&f5yz8v!cmelef^ni8jw1n)ehne5z0g@t44qd9gp6"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-
 
 # Application definition
 
