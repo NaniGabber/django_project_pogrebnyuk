cd5ed6b40f40d8023c287ee9e5ad3dde7b393515
Alexandr Pogrebnyuk <nani.lennauchpage@gmail.com>
2025-11-27 21:15:55 +0200

    fix: slug translate from Ukrainian

--- bazar/models.py
+++ bazar/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+
 # Create your models here.
 
 
@@ -26,9 +27,80 @@ class Section(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            temp = self.title.translate(str.maketrans("абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ","abvgdeejzijklmnoprstufhzcss_y_euaABVGDEEJZIJKLMNOPRSTUFHZCSS_...: Y_EUA"))
-            self.slug = slugify(temp)
-            
+            UKR_TRANSLIT_TABLE = {
+                "а": "a",
+                "б": "b",
+                "в": "v",
+                "г": "h",
+                "ґ": "g",
+                "д": "d",
+                "е": "e",
+                "є": "ie",
+                "ж": "zh",
+                "з": "z",
+                "и": "y",
+                "і": "i",
+                "ї": "i",
+                "й": "i",
+                "к": "k",
+                "л": "l",
+                "м": "m",
+                "н": "n",
+                "о": "o",
+                "п": "p",
+                "р": "r",
+                "с": "s",
+                "т": "t",
+                "у": "u",
+                "ф": "f",
+                "х": "kh",
+                "ц": "ts",
+                "ч": "ch",
+                "ш": "sh",
+                "щ": "shch",
+                "ь": "",
+                "ю": "iu",
+                "я": "ia",
+                "А": "A",
+                "Б": "B",
+                "В": "V",
+                "Г": "H",
+                "Ґ": "G",
+                "Д": "D",
+                "Е": "E",
+                "Є": "Ye",
+                "Ж": "Zh",
+                "З": "Z",
+                "И": "Y",
+                "І": "I",
+                "Ї": "Yi",
+                "Й": "Y",
+                "К": "K",
+                "Л": "L",
+                "М": "M",
+                "Н": "N",
+                "О": "O",
+                "П": "P",
+                "Р": "R",
+                "С": "S",
+                "Т": "T",
+                "У": "U",
+                "Ф": "F",
+                "Х": "Kh",
+                "Ц": "Ts",
+                "Ч": "Ch",
+                "Ш": "Sh",
+                "Щ": "Shch",
+                "Ь": "",
+                "Ю": "Iu",
+                "Я": "Ia",
+            }
+
+            table = str.maketrans(UKR_TRANSLIT_TABLE)
+            slug = self.title.translate(table)
+
+            self.slug = slugify(slug)
+
         super().save(*args, **kwargs)
 
     def __str__(self):
@@ -50,10 +122,14 @@ class Product(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            temp = self.title.translate(str.maketrans("абвгдеёжзийклмнопрстуфхцчшщъыьэюя",
-                                                      "abvgdeejzijklmnoprstufhzcss_y_eua"))
+            temp = self.title.translate(
+                str.maketrans(
+                    "абвгдеёжзийклмнопрстуфхцчшщъыьэюя",
+                    "abvgdeejzijklmnoprstufhzcss_y_eua",
+                )
+            )
             self.slug = slugify(temp)
-            
+
         super().save(*args, **kwargs)
 
     def __str__(self):
