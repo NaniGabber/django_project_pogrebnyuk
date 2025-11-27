e556480cd353e7aa54acfaf5c622122ee329a15a
Alexandr Pogrebnyuk <nani.lennauchpage@gmail.com>
2025-11-27 19:01:22 +0200

    fix: Added csrf trusted origins

--- base/settings.py
+++ base/settings.py
@@ -159,6 +159,9 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD") 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://nani.up.railway.app/",
+]
 
 
 
